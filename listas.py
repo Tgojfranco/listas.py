ee0e96c490e970b079988ebc95a11aa06d57a2ce
JOEL FRANCO MUNOZ <AL02901595@tecmilenio.mx>
2021-10-30 11:35:40 -0500

    .

--- listas.py
+++ listas.py
@@ -1,3 +1,21 @@
-#tuplas 
-#las tuplas no se pueden modificar y las listas si
-s=(1,) #si solo quiero especificar un dato dentro de la tupla tengo que poner coma (,) despues del elemento
+##tuplas 
+##las tuplas no se pueden modificar y las listas si
+#import pprint
+#s=(1,) #si solo quiero especificar un dato dentro de la tupla tengo que poner coma (,) despues del elemento
+#tupla1= ("hola", "Como", "estas")
+#lista_resultados=list(tupla1[1:2]) #cargo la tupla en una lista, si en corchetes especifico el rango solo carga esos datos
+#print(lista_resultados)
+
+#d={1:"valo1",2:"valo2","b":["martillo","tamarindo"]}
+#print(d.keys())
+#print(d.values())
+#d1={
+#    "a":"zanahoria",
+#    2:"limones",
+#    "frutas":12,
+#    "d2":d    
+#    }
+
+#entrada=input("dame una llave: ")
+#pprint.pprint(d[entrada])
+
