9a539cffd1e218020cdb98a8fe7d4efbed767bd2
JOEL FRANCO MUNOZ <AL02901595@tecmilenio.mx>
2021-10-30 09:31:03 -0500

    0

--- listas.py
+++ listas.py
@@ -2,7 +2,7 @@
 lista_test_2=list()
 lista_test=[]
 #asignacion de variables
-lista_str=["hola", "mundo"] #lista de strings 
+lista_str=["hola", "mundo","como","estas"] #lista de strings 
 lista_numero=[1,2,3,4,5,6] #lista de numeros
 lista_float=[1.2,1.3,1.5,2.4]#lista de flotantes
 lista_bool=[True,False,False,True]#valores boolaneos
@@ -10,4 +10,8 @@ lista_bool=[True,False,False,True]#valores boolaneos
 lista_mx=[lista_str,lista_numero,lista_numero] #lista con listas anidadas
 lista_mx2=["hola",159,1.28,True] #lista de todos los tipos de datos
 
-print(len(lista_mx2))
+print(len(lista_mx2)) #indica la longitud de la lista
+print(type(lista_mx2)) #indica si es una lista, tupla, etc...
+
+#indices
+print(lista_mx[-1]) #imprime el ultimo elemento de la lista
