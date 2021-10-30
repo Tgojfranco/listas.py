e5facacafcf53a4acca26845ff18681360bfe2fd
JOEL FRANCO MUNOZ <AL02901595@tecmilenio.mx>
2021-10-30 11:45:41 -0500

    .

--- listas.py
+++ listas.py
@@ -19,3 +19,13 @@
 #entrada=input("dame una llave: ")
 #pprint.pprint(d[entrada])
 
+telefono={
+    "phone_number1": "513-288-0764",
+    "phone_number2": "199-396-3280",
+    "phone_number3": "740-474-0662",
+    "phone_number4": "929-675-8462",
+    "phone_number5": "636-411-2432",
+    "phone_number6": "763-833-0882"
+    }
+print(telefono['phone_number6'])
+telefono['phone_number7']='266-939-9616'
